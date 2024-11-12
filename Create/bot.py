83bef7de46c243b23655ffff6b3ffc0520e151fd
OnePailas <tuwgvfc@gmail.com>
2024-11-12 22:50:50 +0500

    Add files via upload

---
+++ Create/bot.py
@@ -0,0 +1,15 @@
+from javascript import require, On, Once, AsyncTask, off, on
+
+mineflayer = require('mineflayer')
+
+bot = mineflayer.createBot({
+    'host': 'NeMe08.aternos.me',
+    'username': 'Nqowdown',
+    'version': '1.8.9'
+})
+
+@On(bot, 'chat')
+def msgHandler(this, user, message, *args):
+    if user != 'Bot':
+        bot.chat(f'Привет, {user}, как твои дела?')
+
